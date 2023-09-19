284384a28ffa900b189fdf7ff4d6ff60a9a80110
AdamenkoBohdan <adamenko.bogdan@gmail.com>
2023-09-19 20:35:37 +0200

    Add simple test, requirements.txt, update README.md

--- Quizzes/tests.py
+++ Quizzes/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class HomePageTest(TestCase):
+    # Test case for the home page of the application
+
+    def test_home_page_status_code(self):
+        # Test to ensure that the home page is accessible and returns a 200 status code
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 300)
