a382eaf3edc2a988e5e639c1c0cd666afa038f31
AdamenkoBohdan <adamenko.bogdan@gmail.com>
2023-09-23 14:50:24 +0200

    add network

--- Quizzes/models.py
+++ Quizzes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    """Abstract base model with self-updating `created_at` and `updated_at` fields."""
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
