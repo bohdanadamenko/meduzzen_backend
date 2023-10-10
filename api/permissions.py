9e6dfdbc4f83031f77742b79c1a6931b4797714b
AdamenkoBohdan <adamenko.bogdan@gmail.com>
2023-10-11 00:39:47 +0200

    update permissions

--- api/permissions.py
+++ api/permissions.py
@@ -2,7 +2,9 @@ from rest_framework import permissions
 
 
 class IsOwnerOrAdmin(permissions.BasePermission):
-    """Custom permission to only allow owners of an object or admins to edit it."""
+    """
+    Custom permission to only allow owners of an object or admins to edit it.
+    """
 
     def has_object_permission(self, request, view, obj):
         # Read permissions are allowed to any request,
@@ -10,5 +12,12 @@ class IsOwnerOrAdmin(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
 
+        # Instance check for 'owner' attribute and additional check to ensure 'owner' is not None
+        has_owner = hasattr(obj, 'owner') and obj.owner is not None
+
         # Write permissions are only allowed to the owner of the object or to admins.
-        return obj == request.user or obj.owner == request.user or request.user.is_staff
+        return (
+            obj == request.user or
+            (has_owner and obj.owner == request.user) or
+            request.user.is_staff
+        )
